f4dbe060058269b2c012ef8132b4cdf322ae1959
EriikGabriel <erikgabriel.lins@hotmail.com>
2026-02-20 18:35:25 -0300

    feat: add zustand, immer, font, store alias and header

--- src/pages/_app/index.tsx
+++ src/pages/_app/index.tsx
@@ -1,13 +1,18 @@
+import { Header } from "@components/header"
+import { useMode } from "@store/mode"
 import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute('/_app/')({
+export const Route = createFileRoute("/_app/")({
   component: App,
 })
 
 export function App() {
+  const { mode } = useMode()
+
   return (
-    <main className="flex justify-center items-center min-h-dvh">
-      <h1>Hello World</h1>
+    <main className="min-h-dvh">
+      <Header />
+      {mode}
     </main>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
-import path from "node:path"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
@@ -24,6 +24,7 @@ export default defineConfig({
       "@ui": path.resolve(__dirname, "./src/components/ui"),
       "@lib": path.resolve(__dirname, "./src/lib"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@store": path.resolve(__dirname, "./src/store"),
     },
   },
 })
