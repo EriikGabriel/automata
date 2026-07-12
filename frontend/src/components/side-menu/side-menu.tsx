3d2c9a11fde86432b6bda0b398cbb399cbde0f3f
EriikGabriel <erikgabriel.lins@hotmail.com>
2026-07-12 19:30:03 -0300

    refactor: sideMenu conditional rendering and format

--- frontend/src/components/side-menu/side-menu.tsx
+++ frontend/src/components/side-menu/side-menu.tsx
@@ -1,24 +1,22 @@
-import { useMode } from "@store/mode"
-import { useSelection } from "@store/selection"
-import { GeneralOptions } from "./general-options"
-import { SimulationOptions } from "./simulation-options"
-import { StateOptions } from "./state-options"
+import { useMode } from "@store/mode";
+import { useSelection } from "@store/selection";
+import { GeneralOptions } from "./general-options";
+import { SimulationOptions } from "./simulation-options";
+import { StateOptions } from "./state-options";
 
 export function SideMenu() {
-  const selectedStates = useSelection((s) => s.selectedNodeIds)
-  const mode = useMode((s) => s.mode)
+	const selectedStates = useSelection((s) => s.selectedNodeIds);
+	const mode = useMode((s) => s.mode);
 
-  return (
-    <aside className="h-[calc(100vh-64px-28px)] w-1/5 bg-white border-l border-slate-200 text-white p-4">
-      {mode === "automata-str" ? (
-        selectedStates.length > 0 ? (
-          <StateOptions />
-        ) : (
-          <GeneralOptions />
-        )
-      ) : (
-        <SimulationOptions />
-      )}
-    </aside>
-  )
+	return (
+		<aside className="h-[calc(100vh-64px-28px)] w-1/5 bg-white border-l border-slate-200 text-white p-4">
+			{selectedStates.length > 0 ? (
+				<StateOptions />
+			) : mode === "simulate" ? (
+				<SimulationOptions />
+			) : (
+				<GeneralOptions />
+			)}
+		</aside>
+	);
 }
